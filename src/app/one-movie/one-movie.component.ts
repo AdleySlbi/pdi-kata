cd04a3436d8f925af661eb8dd075091131117a0f
AdSlb <adley.salabi@gmail.com>
2022-06-23 12:16:55 +0200

    feat(data) : data integrated inside the wanted component

--- src/app/one-movie/one-movie.component.ts
+++ src/app/one-movie/one-movie.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-one-movie',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./one-movie.component.scss']
 })
 export class OneMovieComponent implements OnInit {
+
+  @Input() film: any;
 
   constructor() { }
 
